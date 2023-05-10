7992a13c51eb1b9e6f3513919433d70304a3ec9e
kafri8889 <kafri8889@gmail.com>
2023-05-10 23:03:25 +0700

    Move tile

--- app/src/main/java/com/anafthdev/a2048/foundation/common/GameEngine.kt
+++ app/src/main/java/com/anafthdev/a2048/foundation/common/GameEngine.kt
@@ -1,16 +1,94 @@
 package com.anafthdev.a2048.foundation.common
 
 import com.anafthdev.a2048.data.Direction
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import timber.log.Timber
 
 class GameEngine {
 	
-	private val _tiles = MutableStateFlow(emptyBoard)
+	private val TILE_SIZE = 4
+	
+	private val _tiles = MutableStateFlow(testBoard)
 	val tiles: StateFlow<Array<Int>> = _tiles
 	
-	fun move(direction: Direction) {
+	// For testing
+	init {
+		CoroutineScope(Dispatchers.IO).launch {
+			delay(3000)
+			
+			withContext(Dispatchers.Main) {
+				move(Direction.Down)
+			}
+		}
+	}
 	
+	fun move(direction: Direction) {
+		val newTiles = tiles.value.clone()
+		val tileRange = 0 until TILE_SIZE
+		
+		when (direction) {
+			Direction.Up -> {
+			
+			}
+			Direction.Down -> {
+				// `(TILE_SIZE - 2)` => Exclude last index
+				for (i in (TILE_SIZE - 2) downTo 0) {
+					for (j in 0 until TILE_SIZE) {
+						var currentIndex = i * TILE_SIZE + j
+						var nextIndex = (i + 1).coerceIn(tileRange) * TILE_SIZE + j
+						var nextTile = newTiles[nextIndex]
+						
+						Timber.i("nextTile ($i, $j): $nextTile")
+						
+						whileLoop@ while (true) {
+							if (nextTile != 0) {
+								newTiles[currentIndex] = newTiles[currentIndex]
+								Timber.i("break `because nextTile != 0`")
+								break@whileLoop
+							}
+
+							newTiles[nextIndex] = newTiles[currentIndex]
+							newTiles[currentIndex] = 0
+							
+							val xNextIndex = nextIndex % TILE_SIZE
+							val yNextIndex = nextIndex / TILE_SIZE
+							
+							val newYNextIndex = yNextIndex + 1
+							
+							Timber.i("next y: $newYNextIndex")
+							
+							if (newYNextIndex >= TILE_SIZE) {
+								Timber.i("break because `newYNextIndex >= TILE_SIZE`")
+								break@whileLoop
+							}
+							
+							currentIndex = nextIndex
+							nextIndex = newYNextIndex * TILE_SIZE + xNextIndex
+							nextTile = newTiles[nextIndex]
+							
+							Timber.i("updated current index: $currentIndex")
+							Timber.i("updated next index: $nextIndex")
+							Timber.i("updated next tile: $nextTile")
+						}
+					}
+				}
+			}
+			Direction.Left -> {
+			
+			}
+			Direction.Right -> {
+			
+			}
+		}
+		
+		_tiles.update { newTiles }
 	}
 	
 	companion object {
@@ -19,12 +97,13 @@ class GameEngine {
 			0, 0, 0, 0,
 			0, 0, 0, 0,
 			0, 0, 0, 0,
+			0, 0, 0, 0,
 		)
 		
 		val testBoard: Array<Int> = arrayOf(
-			0, 8, 2048, 0,
-			16, 0, 32, 512,
-			0, 1024, 64, 4,
+			16, 8, 2048, 0,
+			0, 2, 0, 512,
+			0, 1024, 32, 4,
 			128, 2, 0, 256,
 		)
 	}
--- app/src/main/java/com/anafthdev/a2048/runtime/MainActivity.kt
+++ app/src/main/java/com/anafthdev/a2048/runtime/MainActivity.kt
@@ -3,6 +3,9 @@ package com.anafthdev.a2048.runtime
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.anafthdev.a2048.foundation.common.GameEngine
 import com.anafthdev.a2048.foundation.theme._2048Theme
 import com.anafthdev.a2048.uicomponent.GameBoard
@@ -13,7 +16,13 @@ class MainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 		setContent {
 			_2048Theme {
-				GameBoard(tiles = GameEngine.testBoard)
+				val gameEngine = remember {
+					GameEngine()
+				}
+				
+				val tiles by gameEngine.tiles.collectAsStateWithLifecycle()
+				
+				GameBoard(tiles = tiles)
 			}
 		}
 	}
