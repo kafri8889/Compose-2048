fd8089d757294f4507e50f39a995568dc240ebde
kafri8889 <kafri8889@gmail.com>
2023-05-12 07:16:49 +0700

    UI update

--- app/src/main/java/com/anafthdev/a2048/runtime/MainActivity.kt
+++ app/src/main/java/com/anafthdev/a2048/runtime/MainActivity.kt
@@ -3,9 +3,12 @@ package com.anafthdev.a2048.runtime
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.anafthdev.a2048.data.Direction
 import com.anafthdev.a2048.foundation.common.GameEngine
@@ -14,14 +17,15 @@ import com.anafthdev.a2048.uicomponent.GameBoard
 
 class MainActivity : ComponentActivity() {
 	
+	private lateinit var gameEngine: GameEngine
+	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		
+		gameEngine = GameEngine()
+		
 		setContent {
 			_2048Theme {
-				val gameEngine = remember {
-					GameEngine()
-				}
-				
 				val tiles by gameEngine.tiles.collectAsStateWithLifecycle()
 				val lastAddedTileIndex by gameEngine.lastAddedTileIndex.collectAsStateWithLifecycle()
 				
@@ -29,22 +33,28 @@ class MainActivity : ComponentActivity() {
 					gameEngine.init()
 				}
 				
-				GameBoard(
-					tiles = tiles,
-					lastAddedTileIndex = lastAddedTileIndex,
-					onUp = {
-						gameEngine.move(Direction.Up)
-					},
-					onDown = {
-						gameEngine.move(Direction.Down)
-					},
-					onLeft = {
-						gameEngine.move(Direction.Left)
-					},
-					onRight = {
-						gameEngine.move(Direction.Right)
-					},
-				)
+				Column(
+					horizontalAlignment = Alignment.CenterHorizontally,
+					modifier = Modifier
+						.fillMaxSize()
+				) {
+					GameBoard(
+						tiles = tiles,
+						lastAddedTileIndex = lastAddedTileIndex,
+						onUp = {
+							gameEngine.move(Direction.Up)
+						},
+						onDown = {
+							gameEngine.move(Direction.Down)
+						},
+						onLeft = {
+							gameEngine.move(Direction.Left)
+						},
+						onRight = {
+							gameEngine.move(Direction.Right)
+						},
+					)
+				}
 			}
 		}
 	}
